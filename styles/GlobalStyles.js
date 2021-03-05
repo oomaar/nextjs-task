c5b49fe676e63e0535904d1b93eb793d2575a3a0
Omar <omaar_5@hotmail.com>
2021-03-06 00:44:26 +0200

    add global styles

---
+++ styles/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Helvetica, sans-serif;
+    }
+`;
